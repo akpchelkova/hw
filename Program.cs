c7a971190c21c7fff3679ac502e832b2d45f9317
akpchelkova <annapchelka75@mail.ru>
2024-02-22 17:47:16 +0300

    Добавила код решения

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Введите строки через запятую без пробелов(слова на английском): ");
+        string input = Console.ReadLine();
+
+        // Разбиваем введенные строки по запятой и создаем массив строк
+        string[] inputArray = input.Split(',');
+
+        // Формируем новый массив из строк, длина которых меньше или равна 3 символам
+        string[] resultArray = FilterStrings(inputArray);
+
+        // Выводим результат
+        Console.WriteLine("Результат:");
+        foreach (string str in resultArray)
+        {
+            Console.WriteLine(str);
+        }
+    }
+
+    static string[] FilterStrings(string[] inputArray)
+    {
+        // Используем LINQ для фильтрации строк по длине
+        var result = from str in inputArray
+                     where str.Length <= 3
+                     select str;
+
+        // Преобразуем результат в массив строк
+        return result.ToArray();
+    }
+}
